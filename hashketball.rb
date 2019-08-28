6d9ee9375e1ef6cab62f5d8242437974ce3ee88b
Huy Pham <benjaminhuypham@gmail.com>
2019-08-28 20:10:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,72 @@
 # Write your code here!
 
-
+game_hash = {
+  :home => {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => [
+      {:player => "Alan Anderson"
+       :number => 0
+       :shoe => 16
+       :points => 22
+       :rebounds => 12
+       :assists => 12
+       :steals => 3	
+       :blocks => 1
+       :slam_drunks => 1	
+      },
+      
+      {
+      :player => "Reggie Evans"
+       :number => 30
+       :shoe => 14
+       :points => 12
+       :rebounds => 12
+       :assists => 12
+       :steals => 12	
+       :blocks => 12
+       :slam_drunks => 7	
+      },
+      
+      {:player => "Brook Lopez"
+       :number => 11	
+       :shoe => 17
+       :points => 17
+       :rebounds => 19
+       :assists => 10
+       :steals => 3	
+       :blocks => 1
+       :slam_drunks => 15
+      },
+      
+      {:player => "Brook Lopez"
+       :number => 11	
+       :shoe => 17
+       :points => 17
+       :rebounds => 19
+       :assists => 10
+       :steals => 3	
+       :blocks => 1
+       :slam_drunks => 15
+      },
+    ]
+    
+      
+      
+  :away => {
+    :team_name => "Brooklyn Nets"
+    :colors => ["Black", "White"]
+    :players => [
+      {:number =>
+       :shoe
+       :points
+       :rebounds
+       :assists
+       :steals
+       :blocks
+       :slam_drunks
+  }
+}
 
 
 
